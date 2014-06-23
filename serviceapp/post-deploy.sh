e6150a769abc14d9bb944a19e72e296323c7e715
Raghu <raghu.k@openly.co>
2014-06-23 18:24:34 +0530

    Corrected Forever
    Signed-off-by: Raghu <raghu.k@openly.co>

--- serviceapp/post-deploy.sh
+++ serviceapp/post-deploy.sh
@@ -14,5 +14,5 @@ mkdir temp/tokens
 mkdir temp/failed_emails
 mkdir logs
 echo "Starting the script"
-forever stop src/app.js
-forever start src/app.js -l logs/forever.log -o logs/console.log -e logs/error.log
+forever stop src/app.coffee
+forever start -c coffee src/app.coffee -l logs/forever.log -o logs/console.log -e logs/error.log
